bcaba2658c211e908cd82a67cdb73ec37147e9f3
wu <wu.yuyuan@hotmail.com>
2020-03-12 11:21:40 +0100

    QuestionHelper chinois fini + photos

--- app/src/main/java/com/learn/chinesequiz/QuestionHelper.java
+++ app/src/main/java/com/learn/chinesequiz/QuestionHelper.java
@@ -5,46 +5,46 @@ import java.util.List;
 
 public class QuestionHelper {
 
-    /*
-    public static List<Question> getEasyQuestions() {
+    public static ArrayList<Question> getEasyQuestions() {
         ArrayList<Question> easyList = new ArrayList<>();
-        easyList.add(new Question("Que signifie ce caractère ?", "Voiture", R.drawable.car, 0));
-        easyList.add(new Question("Que signifie ce caractère ?", "Feu", R.drawable.fire, 0));
-        easyList.add(new Question("Que signifie ce caractère ?", "Forêt", R.drawable.forest, 0));
-        easyList.add(new Question("Que signifie ce caractère ?", "Lune", R.drawable.moon, 0));
-        easyList.add(new Question("Que signifie ce caractère ?", "Montagne", R.drawable.mountain, 0));
-        easyList.add(new Question("Que signifie ce caractère ?", "Soleil", R.drawable.sun, 0));
-        easyList.add(new Question("Que signifie ce caractère ?", "Eau", R.drawable.water, 0));
+        easyList.add(new Question("Cette image signifie quel caractère?", "车", R.drawable.car, 0));
+        easyList.add(new Question("Cette image signifie quel caractère?", "火", R.drawable.fire, 0));
+        easyList.add(new Question("Cette image signifie quel caractère?", "森", R.drawable.forest, 0));
+        easyList.add(new Question("Cette image signifie quel caractère?", "月", R.drawable.moon, 0));
+        easyList.add(new Question("Cette image signifie quel caractère?", "山", R.drawable.mountain, 0));
+        easyList.add(new Question("Cette image signifie quel caractère?", "日", R.drawable.sun, 0));
+        easyList.add(new Question("Cette image signifie quel caractère?", "水", R.drawable.water, 0));
         return easyList;
     }
-    */
+
     public static ArrayList<String> getWrongEasyResponses(){
         ArrayList<String> easyWrongResponses = new  ArrayList<>();
-        easyWrongResponses.add("Camion");
-        easyWrongResponses.add("Feuille");
-        easyWrongResponses.add("Ville");
-        easyWrongResponses.add("Ciel");
-        easyWrongResponses.add("Colline");
-        easyWrongResponses.add("Tronc");
-        easyWrongResponses.add("Nourriture");
-        easyWrongResponses.add("Sac");
-        easyWrongResponses.add("Plage");
-        easyWrongResponses.add("Etoile");
+        easyWrongResponses.add("人");
+        easyWrongResponses.add("国");
+        easyWrongResponses.add("干");
+        easyWrongResponses.add("马");
+        easyWrongResponses.add("舌");
+        easyWrongResponses.add("口");
+        easyWrongResponses.add("好");
+        easyWrongResponses.add("比");
+        easyWrongResponses.add("曰");
+        easyWrongResponses.add("吃");
 
         return easyWrongResponses;
     }
 
-    public static ArrayList<Question> getMediumQuestions() {
+    public static List<Question> getMediumQuestions() {
         ArrayList<Question> mediumList = new ArrayList<>();
-        mediumList.add(new Question("Que signifie ce caractère ?", "车", R.drawable.car, 0));
-        mediumList.add(new Question("Que signifie ce caractère ?", "火", R.drawable.fire, 0));
-        mediumList.add(new Question("Que signifie ce caractère ?", "森", R.drawable.forest, 0));
-        mediumList.add(new Question("Que signifie ce caractère ?", "月", R.drawable.moon, 0));
-        mediumList.add(new Question("Que signifie ce caractère ?", "山", R.drawable.mountain, 0));
-        mediumList.add(new Question("Que signifie ce caractère ?", "日", R.drawable.sun, 0));
-        mediumList.add(new Question("Que signifie ce caractère ?", "水", R.drawable.water, 0));
+        mediumList.add(new Question("Que signifie ce caractère ?", "maison", R.drawable.home, 0));
+        mediumList.add(new Question("Que signifie ce caractère ?", "oiseau", R.drawable.bird, 0));
+        mediumList.add(new Question("Que signifie ce caractère ?", "pluie", R.drawable.rain, 0));
+        mediumList.add(new Question("Que signifie ce caractère ?", "pays", R.drawable.countre, 0));
+        mediumList.add(new Question("Que signifie ce caractère ?", "chat", R.drawable.cat, 0));
+        mediumList.add(new Question("Que signifie ce caractère ?", "nuage", R.drawable.cloud, 0));
+        mediumList.add(new Question("Que signifie ce caractère ?", "neige", R.drawable.snow, 0));
         return mediumList;
     }
+
 
     public static ArrayList<String> getWrongMediumResponses(){
         ArrayList<String> mediumWrongResponses = new  ArrayList<>();
@@ -60,6 +60,34 @@ public class QuestionHelper {
         mediumWrongResponses.add("Etoile");
 
         return mediumWrongResponses;
+    }
+
+    public static ArrayList<Question> getHardQuestions() {
+        ArrayList<Question> hardList = new ArrayList<>();
+        hardList.add(new Question("Que signifie ce caractère ?", "gagner", R.drawable.gagner, 0));
+        hardList.add(new Question("Que signifie ce caractère ?", "cuisiner", R.drawable.cuisiner, 0));
+        hardList.add(new Question("Que signifie ce caractère ?", "aimer", R.drawable.aimer, 0));
+        hardList.add(new Question("Que signifie ce caractère ?", "compliquer", R.drawable.compliquer, 0));
+        hardList.add(new Question("Que signifie ce caractère ?", "aveugler", R.drawable.aveugle, 0));
+        hardList.add(new Question("Que signifie ce caractère ?", "voleur", R.drawable.voleur, 0));
+        hardList.add(new Question("Que signifie ce caractère ?", "givre", R.drawable.givre, 0));
+        return hardList;
+    }
+
+    public static ArrayList<String> getWrongHardResponses(){
+        ArrayList<String> hardWrongResponses = new  ArrayList<>();
+        hardWrongResponses.add("silence");
+        hardWrongResponses.add("prochain");
+        hardWrongResponses.add("salle");
+        hardWrongResponses.add("immeuble");
+        hardWrongResponses.add("bimbimbap");
+        hardWrongResponses.add("restaurant");
+        hardWrongResponses.add("appetit");
+        hardWrongResponses.add("assiette");
+        hardWrongResponses.add("fouchette");
+        hardWrongResponses.add("manger");
+
+        return hardWrongResponses;
     }
 
 
